816914943d42322392ad6b9645148682f9cc2704
Jonas Boone <jonas.boone@student.kdg.be>
2022-01-05 17:37:39 +0100

    Added pagination

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ function App() {
   const [page, setPage] = useState<string>('Planets');
 
   return (
+    <>
     <QueryClientProvider client={queryClient}>
     <div className="App">
       <h1>Star Wars Information</h1>
@@ -22,6 +23,7 @@ function App() {
       </div>
     </div>
     </QueryClientProvider>
+    </>
   );
 }
 
--- src/components/Planets.tsx
+++ src/components/Planets.tsx
@@ -1,27 +1,30 @@
 import { useQuery } from "react-query";
 import axios from "axios";
 import Planet from "./Planet";
+import { useState } from "react";
 
 
 function Planets() {
+  const [page, setPage] = useState<number>(1);
 
-  async function fetchPlanets() {
+  async function fetchPlanets(page: number) {
     try {
-      const res = await axios.get('https://swapi.dev/api/planets');
+      const res = await axios.get(`https://swapi.dev/api/planets/?page=${page}`);
+      console.log(res.data)
       return res.data;
     } catch (err: any) {
       throw new Error(err);
     }
   }
 
-  const {data, status} = useQuery('planets', fetchPlanets);
-  console.log(status)
-
-
+  const {data, status} = useQuery(['planets', page], () => fetchPlanets(page));
 
   return (
     <div>
       <h2>Planets</h2>
+      {page > 1 && <button onClick={() => setPage(prevPage => prevPage - 1)}>Previous page</button>}
+      <button onClick={() => setPage(prevPage => prevPage + 1)}>Next page</button>
+
       {status === 'loading' && <p>Loading...</p>}
       {status === 'error' && <p>Something went wrong while fetching the data...</p>}
       {status === 'success' && data.results.map((planet: any) => <Planet key={planet.url} name={planet.name} population={planet.population} terrain={planet.terrain} />)}
